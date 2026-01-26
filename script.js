6195e96e3bc3881757da8c482f86ae271491f323
Alisa Leung <alisal.singyee@gmail.com>
2026-01-25 22:38:00 -0800

    session 3

--- script.js
+++ script.js
@@ -1,60 +1,70 @@
+/*
+to-do:
+- checkbox for each tasks that is stored in local storage
+- maybe update api completed status
+- css class that changes appearance based on completion
+*/
+
+//defines html elements
 let taskButton = document.getElementById("taskButton");
 let taskList = document.getElementById("taskList");
 let taskInput = document.getElementById("taskInput");
-let listButtons = document.querySelectorAll("li button");
+
+//variables for editing tasks
 let editingTask;
 let editingTaskId;
 
-//save tasks with localstorage
-//save tasks each time tasks are updated
-let tasksJson = `[
-"do homeworks",
-"do chores"
-]`;
-
 let tasks = [];
-let newTasks = JSON.parse(tasksJson);
-JSON.stringify(tasksJson)
-for (let task of newTasks){
-    addTask(task);
-}
+
+//adds tasks from json file
+loadTasks();
 
 taskButton.addEventListener("click", updateUserTask);
 
-//add edit button to tasks
 
 function addTask(name){
     let newTask = document.createElement("li");
 
+    //updates array
+    //creates a unique id like: 36b8f84d-df4e-4d49-b662-bcde71a8764f
     let uuid = crypto.randomUUID();
-    // 36b8f84d-df4e-4d49-b662-bcde71a8764f
-
-    newTask.textContent = name + " ";
     tasks.push({
         id: uuid,
         name
     });
+    
+    //updates html
+    newTask.textContent = name + " ";
     newTask.append(...createButtons(newTask, uuid));
-
     taskList.appendChild(newTask);
 }
 
-function updateUserTask(){
+//any changes to tasks
+async function updateUserTask(){
     if (taskButton.innerHTML === "Add Task"){ 
         addTask(taskInput.value);
+        await fetch("https://dummyjson.com/todos/add", {
+            method: "POST",
+            body: JSON.stringify({
+                todo: taskInput.value,
+                headers: { 'Content-Type': 'application/json' },
+                completed: false,
+                userId: 1
+            })
+        });
     } else if (taskButton.innerHTML === "Save Task"){
         editingTask.textContent = taskInput.value + " ";
+        //checks task array for a task object with an id that matches the task we're
+        //currently editing and sets the name of it to the task input value
         tasks.find(a => a.id === editingTaskId).name = taskInput.value;
         editingTask.append(...createButtons(editingTask, editingTaskId));
         taskButton.innerHTML = "Add Task";
     }
     taskInput.value = "";
+    saveTasks();
 }
 
-function removeTask(event){
-    event.target.parentElement.remove();
-}
-
+//consolidates delete and edit button functionalities + their creation into one function
 function createButtons(newTask, id){
     let editButton = document.createElement("button");
     editButton.addEventListener("click", () => {
@@ -67,6 +77,7 @@ function createButtons(newTask, id){
 
     let deleteButton = document.createElement("button");
     deleteButton.addEventListener("click", () => {
+        //does the same as the line below
         /*let taskIndex = -1;
         for (let i = 0; i < tasks.length; i++){
             if (tasks[i].id === id){
@@ -74,11 +85,44 @@ function createButtons(newTask, id){
                 break;
             }
         }*/
+        //.findIndex() returns the index, whereas .find() returns the object itself
         let taskIndex = tasks.findIndex(a => a.id === id);
+        //removes 1 element starting from taskIndex in the tasks array
         tasks.splice(taskIndex, 1);
         newTask.remove();
+        saveTasks();
     });
     deleteButton.textContent = "Delete";
 
     return [editButton, deleteButton];
 }
+
+function saveTasks(){
+    let tasksJson = JSON.stringify(tasks);
+    localStorage.tasks = tasksJson;
+}
+
+function loadTasks(){
+    let currentTasks = localStorage.getItem("tasks");
+    let newTasks = JSON.parse(currentTasks);
+    if (newTasks != null){
+        for (let task of newTasks){
+            addTask(task.name);
+        }
+    }
+}
+
+let downloadButton = document.getElementById("download");
+downloadButton.addEventListener("click", async () => {
+    // Promise<Response>
+    // GET
+    // POST PUT
+    let response = await fetch("https://dummyjson.com/todos", {
+        method: "GET"
+    });
+    let responseJson = await response.json();
+    for (let task of responseJson.todos){
+        addTask(task.todo);
+    }
+});
+
